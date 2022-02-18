e9ed21997ce9adcbbbef424a115968641e95bca2
Suelen Arruda <suelenonyrius@gmail.com>
2022-02-19 00:12:56 +0100

    adiciona test do src da img e finaliza o requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+const { render, screen } = require('@testing-library/react');
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+
+test('Teste se a página contém um heading `h2` com o texto `About Pokédex`', () => {
+  renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />); // acessando o historico
+  history.push('/about'); // colocando uma rota errada no historico
+  const aboutPokedex = screen.getByRole('heading', { name: /about pokédex/i, level: 2 });
+
+  expect(aboutPokedex).toBeInTheDocument();
+});
+
+test('Teste se a página contém a seguinte imagem de uma Pokédex: `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`', () => {
+  renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />); // acessando o historico
+  history.push('/about'); // colocando uma rota errada no historico
+  const imgPokedex = screen.getByRole('img', { name: /pokédex/i });
+  expect(imgPokedex).toBeInTheDocument();
+  expect(imgPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
