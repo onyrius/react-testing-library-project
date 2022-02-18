c08ab4fd5ccf59fd39ad0724a3833ca7286980f5
Suelen Arruda <suelenonyrius@gmail.com>
2022-02-18 21:10:43 +0100

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+const { render, screen } = require('@testing-library/react');
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+test('Teste se o primeiro link possui o texto `Home`', () => {
+  renderWithRouter(<App />);
+  const homeEl = screen.getByRole('link', { name: /home/i });
+  expect(homeEl).toBeInTheDocument();
+});
+
+test('Teste se o topo da aplicação hà um conjunto fixo de links de navegação.', () => {
+  renderWithRouter(<App />);
+  const aboutEl = screen.getByRole('link', { name: /about/i });
+  expect(aboutEl).toBeInTheDocument();
+});
+test('Teste se é redirecionada para a página de `Pokémons Favoritados`', () => {
+  renderWithRouter(<App />);
+  const favEl = screen.getByRole('link', { name: /favorite pokémons/i });
+  expect(favEl).toBeInTheDocument();
+});
